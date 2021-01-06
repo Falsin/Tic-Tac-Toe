87a6aca24f02edc4a16339558d3a6e8f85e83937
Falsin <greenldend@gmail.com>
2021-01-07 00:11:21 +0700

    fixed gameControl function

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const player = (number, color) => {
 }
 
 const player1 = (() => {
-  let fields = []
+  let fields = {};
 
   return function(name) {
     const {playerNumber, playerColor} = player(1, 'blue');
@@ -17,7 +17,7 @@ const player1 = (() => {
 })()
 
 const player2 = (() => {
-  let fields = [];
+  let fields = {};
 
   return function(name) {
     const {playerNumber, playerColor} = player(2, 'red');
@@ -30,37 +30,35 @@ let gameBoard = (prop, player) => {
   prop.background = player.playerColor;
 }
 
-/* let obj; */
+let obj;
 
 let gameControl = (() => {
-  let currentPlayer = player1();
+  let player = player1();
   let selectedCells = [];
 
   return function(item) {
     if(!selectedCells.includes(item)) {
-      gameBoard(item.style, currentPlayer)
-      currentPlayer = (currentPlayer.playerNumber == 1) ? player2() : player1();
+      gameBoard(item.style, player)
       selectedCells.push(item);
 
       for (let i = 0; i < item.classList.length; i++) {
         let className = item.classList[i];
-        if(className.includes('row')) {
-          let rows = board.querySelectorAll(`.${className}`);
-          currentPlayer.fields.push(rows)
-        } else if(className.includes('column')) {
-          let columns = board.querySelectorAll(`.${className}`);
-          currentPlayer.fields.push(columns);
-        } else {
-          let diagonals = board.querySelectorAll(`.${className}`);
-          currentPlayer.fields.push(diagonals);
+
+        if(player.fields[className] === undefined) {
+          player.fields[className] = 0;
+        }
+
+        player.fields[className]++;
+
+        if(player.fields[className] == 3) {
+          alert(`${player.playerNumber} win!`);
         }
       }
 
+      obj = player.fields;
+      player = (player.playerNumber == 1) ? player2() : player1();
 
-
-      
     }
-    /* obj = currentPlayer.fields; */
   }
 
 })()
@@ -68,93 +66,3 @@ let gameControl = (() => {
 cells.forEach(item => {
   item.addEventListener('mousedown', () => gameControl(item))
 })
-
-/* function showValidFields() {
-  for (let i = 0; i < obj.length; i++) {
-    for (let j = 0; j < obj[i].length; j++) {
-      obj[i][j].style.background = "yellow";
-    }
-  }
-} */
-
-
-   /*  for (let i = 0; i < item.classList.length; i++) {
-      let className = item.classList[i];
-      if(className.includes('row')) {
-        let rows = board.querySelectorAll(`.${className}`);
-        currentPlayer.validField.push(rows)
-        rows.push(rows1);
-      } else if(className.includes('column')) {
-        let columns = board.querySelectorAll(`.${className}`);
-        columns.push(columns1);
-      } else {
-        let diagonals = board.querySelectorAll(`.${className}`);
-        diagonals.push(diagonals1);
-      }
-    } */
-
-    /* for (let i = 0; i < item.classList.length; i++) {
-      let className = item.classList[i];
-      if(className.includes('row')) {
-        let rows1 = board.querySelectorAll(`.${className}`);
-        rows.push(rows1);
-      } else if(className.includes('column')) {
-        let columns1 = board.querySelectorAll(`.${className}`);
-        columns.push(columns1);
-      } else {
-        let diagonals1 = board.querySelectorAll(`.${className}`);
-        diagonals.push(diagonals1);
-      }
-    }
-  }
-})()
-
-cells.forEach(item => {
-  item.addEventListener('mousedown', () => gameControl(item))
-})
-
-
-/* let gameBoard = (() => {
-  const board = document.getElementById('gameBoard');
-  let cells = [...board.children];
-
-  let rows = [];
-  let columns = [];
-  let diagonals = [];
-
-  cells.forEach(item => {
-    item.addEventListener('mousedown', () => {
-      console.dir(item);
-      for (let i = 0; i < item.classList.length; i++) {
-        let className = item.classList[i];
-        if(className.includes('row')) {
-          let rows1 = board.querySelectorAll(`.${className}`);
-          rows.push([...rows1]);
-        } else if(className.includes('column')) {
-          let columns1 = board.querySelectorAll(`.${className}`);
-          columns.push([...columns1]);
-        } else {
-          let diagonals1 = board.querySelectorAll(`.${className}`);
-          diagonals.push([...diagonals1]);
-        }
-      }
-    })
-  })
-
-  return {
-    rows,
-    columns,
-    diagonals
-  }
-})();
-
-let artem = player1('Artem');
-
-
-function changeBackground() {
-  let array = gameBoard.rows[0].concat(gameBoard.columns[0], gameBoard.diagonals[0]);
-  array.forEach(item => {
-    item.style.background = "red";
-  })
-} */
-
