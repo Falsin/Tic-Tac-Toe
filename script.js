b41775173d8f947451ebf35e7b4b95183fe45d05
Falsin <greenldend@gmail.com>
2021-01-07 13:28:30 +0700

    changed player functions

--- script.js
+++ script.js
@@ -1,36 +1,65 @@
+const greetingBlock = document.getElementById('greetingBlock');
+const choosePlayer = document.getElementById('choosePlayer');
+const buttons = choosePlayer.querySelectorAll('button');
+const startButton = document.getElementById('start');
+const names = document.querySelectorAll('input');
+
+const container = document.getElementById('container');
 const board = document.getElementById('gameBoard');
 const cells = [...board.children];
 
-const player = (number, color) => {
-  const playerNumber = number;
-  let playerColor = color;
-  return {playerNumber, playerColor};
-}
 
-const player1 = (() => {
+/* buttons.forEach(item => {
+  item.addEventListener('mousedown', (e) => {
+    const parentNode = item.parentNode.parentNode;
+    const selectedPlayer = parentNode.classList[0];
+    console.log(selectedPlayer);
+  })
+}) */
+
+
+
+startButton.addEventListener('mousedown', () => {
+  greetingBlock.style.display = 'none';
+  container.style.display = 'grid';
+
+  for (let i = 0; i < names.length; i++) {
+    const parentNode = names[i].parentNode.parentNode;
+    const player = parentNode.classList[0];
+    console.log(names[i].value);
+    if(player == 'player1' && names[i].value != '') {
+      player1(names[i].value);
+    } else if(player == 'player2' && names[i].value != '') {
+      player2(names[i].value);
+    }
+  }
+})
+
+let player1 = (() => {
+  let defaultName = 'player 1';
+  let color = 'blue';
   let fields = {};
+  const number = 1;
 
-  return function(name) {
-    const {playerNumber, playerColor} = player(1, 'blue');
-    return {playerNumber, playerColor, fields}
+  return function (name = defaultName) {
+    return {fields, name, color, number}
   }
 })()
 
-const player2 = (() => {
+let player2 = (() => {
+  let defaultName = 'player 2';
+  let color = 'red';
   let fields = {};
+  const number = 2;
 
-  return function(name) {
-    const {playerNumber, playerColor} = player(2, 'red');
-    return {playerNumber, playerColor, fields}
+  return function (name = defaultName) {
+    return {fields, name, color, number}
   }
-
 })()
 
 let gameBoard = (prop, player) => {
-  prop.background = player.playerColor;
+  prop.background = player.color;
 }
-
-let obj;
 
 let gameControl = (() => {
   let player = player1();
@@ -51,12 +80,11 @@ let gameControl = (() => {
         player.fields[className]++;
 
         if(player.fields[className] == 3) {
-          alert(`${player.playerNumber} win!`);
+          alert(`${player.name} win!`);
         }
       }
 
-      obj = player.fields;
-      player = (player.playerNumber == 1) ? player2() : player1();
+      player = (player.number == 1) ? player2() : player1();
 
     }
   }
