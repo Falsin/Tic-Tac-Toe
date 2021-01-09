910059ca17502b420fc001d8fcee94f5f5bef209
Falsin <greenldend@gmail.com>
2021-01-09 16:32:42 +0700

    setting behavior of the restartBlock and gameContol function

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const container = document.getElementById('container');
 const board = document.getElementById('gameBoard');
 const cells = [...board.children];
 
+const restart = document.getElementById('restart');
+const title = restart.querySelector('h2');
+const img = restart.querySelector('img'); 
+
 const body = document.querySelector('body');
 const mainBlocks = [...body.children].slice(0, 3);
 
@@ -20,7 +24,6 @@ const player = (name, color, number, fields) => {
 let player1 = player('player 1', 'blue', 1, {});
 
 let player2 = player('player 2', 'red', 2, {});
-
 
 startButton.addEventListener('mousedown', () => {
   mainBlocks.forEach(item => item.classList.add('off'))
@@ -47,31 +50,51 @@ let gameControl = (() => {
   let selectedCells = [];
 
   return function(item) {
-    if(!selectedCells.includes(item)) {
+    if(item === undefined) {
+      selectedCells = [];
+      player = player1;
+    } else if(!selectedCells.includes(item)) {
       gameBoard(item.style, player)
       selectedCells.push(item);
 
       for (let i = 0; i < item.classList.length; i++) {
         let className = item.classList[i];
 
-        if(player.fields[className] === undefined) {
+        if(!player.fields[className]) {
           player.fields[className] = 0;
         }
-
         player.fields[className]++;
 
         if(player.fields[className] == 3) {
-          mainBlocks[mainBlocks.length - 1].classList.remove('off');
+          restart.classList.remove('off');
+          title.textContent = `${player.name} won!`
         }
       }
 
       player = (player.number == 1) ? player2 : player1;
 
     }
+    return {selectedCells};
   }
 
 })()
 
 cells.forEach(item => {
   item.addEventListener('mousedown', () => gameControl(item))
+})
+
+img.addEventListener('mousedown', () => {
+  mainBlocks.forEach(item => {
+    item.classList.add('off');
+  })
+  container.classList.remove('off');
+
+  cells.forEach(item => {
+    item.style.background = 'white';
+  })
+
+  player1.fields = {};
+  player2.fields = {};
+
+  gameControl();
 })
