db0020e0134d1e7bc9eeeda75682271e64acb6cb
Falsin <greenldend@gmail.com>
2021-01-10 10:45:26 +0700

    setting AI behavior

--- script.js
+++ script.js
@@ -17,8 +17,6 @@ const img = restart.querySelector('img');
 const body = document.querySelector('body');
 const mainBlocks = [...body.children].slice(0, 3);
 
-
-
 const player = (name, color, number, fields) => {
   return {name, color, number, fields}
 }
@@ -31,6 +29,7 @@ buttons.forEach(item => {
   item.addEventListener('mousedown', () => {
     const player = item.parentNode.parentNode.classList[0];
     const mode = item.textContent;
+    const input = item.parentNode.parentNode.querySelector('input');
     const srcImg = (mode == 'Human') ? '/images/personIcon.png':
                                        '/images/robot.jpeg';
 
@@ -42,32 +41,49 @@ buttons.forEach(item => {
       player2.src = srcImg;
     }
 
-    if (mode = 'AI') {
-      
+    if (mode == 'AI') {
+      input.readOnly = true;
+      input.value = 'AI';
+    } else {
+      input.readOnly = false;
+      input.value = '';
     }
   })
 });
 
 startButton.addEventListener('mousedown', () => {
-  mainBlocks.forEach(item => item.classList.add('off'))
-  container.classList.remove('off');
-
-  for (let i = 0; i < names.length; i++) {
-    const parentNode = names[i].parentNode.parentNode;
-    const player = parentNode.classList[0];
-
-    if(player == 'player1' && names[i].value != '') {
-      player1.name = names[i].value;
-    } else if(player == 'player2' && names[i].value != '') {
-      player2.name = names[i].value;
+  if(player1.mode && player2.mode) {
+    mainBlocks.forEach(item => item.classList.add('off'))
+    container.classList.remove('off');
+  
+    for (let i = 0; i < names.length; i++) {
+      const parentNode = names[i].parentNode.parentNode;
+      const player = parentNode.classList[0];
+  
+      if(player == 'player1' && names[i].value != '') {
+        player1.name = names[i].value;
+      } else if(player == 'player2' && names[i].value != '') {
+        player2.name = names[i].value;
+      }
     }
+  
+    players[0].textContent = player1.name;
+    players[1].textContent = player2.name;
+  
+    images[0].src = player1.src;
+    images[1].src = player2.src;
+
+
+
+
+    if(player1.mode == 'AI') {
+      const choice = robotChoice(0, cells.length - 1)
+      gameControl(cells[choice]);
+    }
+  } else {
+    alert('You must choose the player mode!')
   }
 
-  players[0].textContent = player1.name;
-  players[1].textContent = player2.name;
-
-  images[0].src = player1.src;
-  images[1].src = player2.src;
 })
 
 let gameBoard = (prop, player) => {
@@ -101,12 +117,21 @@ let gameControl = (() => {
       }
 
       player = (player.number == 1) ? player2 : player1;
-
+      
+      if(player.mode == 'AI') {
+        let choice;
+        do {
+          choice = robotChoice(0, cells.length - 1);
+        } while (selectedCells.includes(cells[choice]))
+        gameControl(cells[choice]);
+      }
     }
-    return {selectedCells};
   }
-
 })()
+
+function robotChoice(minItem, maxItem) {
+  return Math.round(Math.random() * (maxItem - minItem + 1) + minItem - 0.5);
+}
 
 cells.forEach(item => {
   item.addEventListener('mousedown', () => gameControl(item))
@@ -125,5 +150,8 @@ img.addEventListener('mousedown', () => {
   player1.fields = {};
   player2.fields = {};
 
+/*   if(player1.mode ) {
+
+  } */
   gameControl();
 })
